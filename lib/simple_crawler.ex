4810fd04756e6adbea4f26d14b9804209531c742
KentaJerry <kenta.h@thewaggle.co.jp>
2022-07-13 15:40:09 +0900

    get_url_list,get_page_textの作成

--- lib/simple_crawler.ex
+++ lib/simple_crawler.ex
@@ -1,18 +1,30 @@
 defmodule SimpleCrawler do
-  @moduledoc """
-  Documentation for `SimpleCrawler`.
-  """
+  def get_url(url) do
+    html = HTTPoison.get!(url)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SimpleCrawler.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    {:ok, document} = Floki.parse_document(html.body)
+    document |> Floki.find("a") |> Floki.attribute("href")
   end
+  def get_url_list(url_list) do
+    domain = "https://thewaggletraining.github.io/"
+
+    list =
+      for url <- url_list do
+        get_url(url)
+        |> Enum.filter(& String.starts_with?(&1,domain))
+      end
+      List.flatten(list)
+    end
+
+ def get_page_text(url_list) do
+  for url <- url_list do
+     html = HTTPoison.get!(url)
+
+     Floki.parse_document!(html.body)
+     |>Floki.find("body")
+     |>Floki.text()
+     |>String.replace([" ","\n"],"")
+   end
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-1defmodule SimpleCrawler.MixProject do
+defmodule SimpleCrawler.MixProject do
   use Mix.Project
 
   def project do

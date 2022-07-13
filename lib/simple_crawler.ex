4def8bb896edcb6095e1cbc22f7e1403580853fa
KentaJerry <kenta.h@thewaggle.co.jp>
2022-07-13 11:35:29 +0900

    simple_crawlerの作成

---
+++ lib/simple_crawler.ex
@@ -0,0 +1,18 @@
+defmodule SimpleCrawler do
+  @moduledoc """
+  Documentation for `SimpleCrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
